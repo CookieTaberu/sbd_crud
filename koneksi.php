a1a8bec348e6c1bb056fe2fef842a57c9bf136f1
TrianDii <134402856+CookieTaberu@users.noreply.github.com>
2024-11-26 22:24:48 +0700

    testing

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,10 @@
-
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'projek_sbd';
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Koneksi bodo". $conn->connect_error);
+}
+?>
